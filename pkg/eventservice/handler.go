3e572a84cdc801b6b1327191149f61d8629ec1bd
MrSwartz <usser@live.ru>
2023-06-03 21:01:29 +0300

    improved endpoint

--- pkg/eventservice/handler.go
+++ pkg/eventservice/handler.go
@@ -2,7 +2,6 @@ package eventservice
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -41,7 +40,7 @@ func (h *Handler) storeEvents(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		logrus.Errorf("can't read body from request: %v", err)
-		sendResponse(w, err)
+		sendResponse(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -50,14 +49,19 @@ func (h *Handler) storeEvents(w http.ResponseWriter, r *http.Request) {
 	var incomingData []EventModel
 	if err := json.Unmarshal(body, &incomingData); err != nil {
 		logrus.Errorf("can't unmarshal body request: %v", err)
-		sendResponse(w, err)
+		sendResponse(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	ip, ok := utils.ExtractIpAddr(r)
 	if !ok {
+		// тут ip должен вытягивается с учётом балансировщика нагрузки
+		// странный баг, если не удалось вытащить ip из запроса,
+		// вероятно хэдеры были модифицированы
+		// если проблема не в хэдерах, то что то совсем не то
+		// и сервис не должен ничего сохранять
 		logrus.Warnf("can't extract ip addr from response: %v", r.Header)
-		// todo
+		sendResponse(w, "can't extract ip addr", http.StatusInternalServerError)
 	}
 
 	data := make([]service.ServiceEventModel, 0, len(incomingData))
@@ -66,14 +70,27 @@ func (h *Handler) storeEvents(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println(data)
 
-	err1 := h.Service.Insert(r.Context(), data)
-	logrus.Infof("request processed, error: %v", err1)
-	sendResponse(w, err1)
+	err = h.Service.Insert(r.Context(), data)
+	logrus.Infof("request processed, error: %v", err)
+	sendResponse(w, err.Error(), http.StatusInternalServerError)
 }
 
 func (h *Handler) health(w http.ResponseWriter, r *http.Request) {
 	err := h.Service.Ping(r.Context())
-	sendResponse(w, err)
+
+	var msg string
+	var code int
+
+	switch err {
+	case nil:
+		msg = "ok"
+		code = http.StatusOK
+	default:
+		msg = err.Error()
+		code = http.StatusInternalServerError
+	}
+
+	sendResponse(w, msg, code)
 }
 
 type HttpResponse struct {
@@ -81,24 +98,7 @@ type HttpResponse struct {
 	Message string `json:"message"`
 }
 
-func sendResponse(w http.ResponseWriter, err error) {
-	var msg string
-	var code int
-
-	if err != nil {
-		var syntax = &json.SyntaxError{}
-		switch {
-		case errors.As(err, &syntax):
-			code = http.StatusBadRequest
-		default:
-			code = http.StatusInternalServerError
-		}
-		msg = err.Error()
-	} else {
-		msg = "ok"
-		code = http.StatusOK
-	}
-
+func sendResponse(w http.ResponseWriter, msg string, code int) {
 	rsp := HttpResponse{
 		Status:  http.StatusText(code),
 		Message: msg,
